f512079da0883dc0946db49f9fa07d44f8fd946b
unixkiwi <unixkiwi77@outlook.com>
2026-06-07 19:04:03 +0200

    feat: add animated rotation to refresh button in timetable screen + some cool new style

--- app/src/main/java/de/unixkiwi/betterschool/ui/timetable/TimetableScreen.kt
+++ app/src/main/java/de/unixkiwi/betterschool/ui/timetable/TimetableScreen.kt
@@ -1,5 +1,11 @@
 package de.unixkiwi.betterschool.ui.timetable
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,12 +18,18 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialShapes
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -54,6 +66,17 @@ private fun TimetableScreen(
     onUpdateBtnClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val infiniteRotation = rememberInfiniteTransition(label = "infiniteRotation")
+    val rotation by infiniteRotation.animateFloat(
+        0F,
+        360F,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1250, delayMillis = 200, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "rotation"
+    )
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -74,7 +97,13 @@ private fun TimetableScreen(
                         Text("Timetable")
                 },
                 navigationIcon = {
-                    IconButton(onClick = {}) {
+                    IconButton(
+                        onClick = {}, shape = MaterialShapes.Cookie4Sided.toShape(),
+                        colors = IconButtonDefaults.iconButtonColors().copy(
+                            containerColor = MaterialTheme.colorScheme.secondary,
+                            contentColor = MaterialTheme.colorScheme.onSecondary
+                        )
+                    ) {
                         Icon(
                             painter = painterResource(R.drawable.settings_filled),
                             contentDescription = null
@@ -82,13 +111,25 @@ private fun TimetableScreen(
                     }
                 },
                 actions = {
-                    IconButton(onClick = onUpdateBtnClick) {
+                    IconButton(
+                        onClick = onUpdateBtnClick,
+                        shape = MaterialShapes.Cookie9Sided.toShape(),
+                        colors = IconButtonDefaults.iconButtonColors().copy(
+                            containerColor = MaterialTheme.colorScheme.secondary,
+                            contentColor = MaterialTheme.colorScheme.onSecondary
+                        ),
+                        modifier = Modifier.graphicsLayer {
+                            if (uiState.isLoadingWithData()) rotationZ = rotation
+                        }
+                    ) {
                         Icon(
                             painter = painterResource(R.drawable.ic_refresh_rounded_24dp),
                             contentDescription = null
                         )
                     }
-                }
+                },
+                colors = TopAppBarDefaults.topAppBarColors()
+                    .copy(containerColor = MaterialTheme.colorScheme.surfaceContainer)
             )
         }
     ) { innerPad ->
