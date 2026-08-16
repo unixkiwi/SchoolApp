bfb70b5437f27050ad83d6fd45b6132acd7d6e9b
unixkiwi <unixkiwi77@outlook.com>
2026-08-16 17:25:38 +0200

    fix: deps in other build variants

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,9 +79,11 @@ dependencies {
     implementation(libs.timber)
     ksp(libs.hilt.android.compiler)
     testImplementation(libs.junit)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(platform(libs.androidx.compose.bom))
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
